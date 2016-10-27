acf8bd278b75d46340bd6b4a833de6cf296ca0a0
andersonrss <andersonrufino2007@gmail.com>
2016-10-27 18:08:37 -0300

    Olá, Carlos. Alterei nosso estado.

--- src/simplesimplex/SimpleSimplex.java
+++ src/simplesimplex/SimpleSimplex.java
@@ -15,7 +15,7 @@ public class SimpleSimplex {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Olá, Carlos");
     }
     
 }
